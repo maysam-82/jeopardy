63347cbd5bba8d88019296ebdcccbfdd4eb763e6
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-07-18 00:28:04 +1000

    Updated style for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<div className="header">
-			<Link to="/">Home</Link>
+		<div className="header pb-2 pt-2 pl-0 pr-0 border-bottom mb-2 font-weight-bold ">
+			<Link to="/" className="badge badge-secondary p-2">
+				Home
+			</Link>
 		</div>
 	);
 };
